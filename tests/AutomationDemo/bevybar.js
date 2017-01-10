301e66568b1f4ef1b6710177c70259cac760add9
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-01-10 15:31:42 -0500

    travis ci

--- tests/AutomationDemo/bevybar.js
+++ tests/AutomationDemo/bevybar.js
@@ -1,17 +1,12 @@
 module.exports = {
   '@tags': ['bevybar'],
-  'Go to Google': function (browser) {
+  'Go to bevybar': function (browser) {
     browser
       .url('https://www.bevybar.com.ar/')
       .waitForElementVisible('body', 1000)
       .pause(1000)
       .click('#Layer_1')
-      .pause(3000)
-      .setValue('#customer-email', 'testautomation5@zxvtest.com')
-      .setValue('#customer-password', 'password1')
-      .pause('1000')
-      .click('#customer_login > input[type="submit"]:nth-child(5)');
-    browser.pause(5000);
+      .pause(3000);
   },
   'Browser quit': function (browser) {
     browser
