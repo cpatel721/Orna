737998b5c0b09061bf63c459ef2a0cf5972b474d
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-01-10 14:25:07 -0500

    beerclub

---
+++ tests/AutomationDemo/beerclub.js
@@ -0,0 +1,20 @@
+module.exports = {
+  '@tags': ['beer-club'],
+  'Go to Google': function (browser) {
+    browser
+      .url('https://testing.beerhawk.co.uk/beer-club/')
+      .pause(1000)
+      .click('#Layer_1')
+      .pause(3000)
+      .setValue('#customer-email', 'testautomation5@zxvtest.com')
+      .setValue('#customer-password', 'password1')
+      .pause('1000')
+      .click('#customer_login > input[type="submit"]:nth-child(5)');
+    browser.pause(5000);
+  },
+  'Browser quit': function (browser) {
+    browser
+    .end();
+  }
+
+};
